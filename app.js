0bd863819c1bfafe1638a13aa154ce3e9357d9f0
Lawrence <143608054+llawrencee@users.noreply.github.com>
2023-10-20 00:26:07 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const navigationBar = document.querySelector('.main-navigation')
+const navigationList = document.querySelector('.navigation-list')
+const navigationLogo = navigationBar.querySelector('.navigation-logo img')
+const navigationOpen = document.getElementById('menu-open')
+const navigationClose = document.getElementById('menu-close')
+
+window.onscroll = () => {
+  if (
+    document.body.scrollTop >= 80 ||
+    document.documentElement.scrollTop >= 80
+  ) {
+    navigationBar.classList.add('scrolled')
+  } else {
+    navigationBar.classList.remove('scrolled')
+  }
+}
+
+navigationOpen.onclick = () => {
+  navigationList.classList.add('open')
+}
+
+navigationClose.onclick = () => {
+  navigationList.classList.remove('open')
+}
+
+new TypeIt('#typing', {
+  speed: 100,
+  deleteSpeed: 45,
+  loop: true,
+  loopDelay: 300,
+})
+  .type('weekly.')
+  .pause(1200)
+  .delete(7)
+  .pause(300)
+  .type('quality.')
+  .pause(1200)
+  .delete(8)
+  .pause(300)
+  .type('security.')
+  .pause(1200)
+  .delete(9)
+  .pause(300)
+  .go()
